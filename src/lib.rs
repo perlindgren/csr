62c972040718e7dd1cd76d34917c8978253d816d
pawdzi-7 <pawdzi-7@student.ltu.se>
2024-08-08 23:09:46 +0200

    WiP

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,14 @@
 ///
 #[macro_export]
 macro_rules! csrr {
-    ($csr_number:literal) => {
+    ($csr_number:expr) => {
         /// Reads the CSR
         {
             let r: usize;
             core::arch::asm!(
-                concat!("csrr {0}, ", stringify!($csr_number)),
-                out(reg) r
+                concat!("csrr {0}, {csr}"),
+                out(reg) r,
+                csr = const $csr_number
             );
             r
         }
@@ -22,24 +23,27 @@ macro_rules! csrr {
 
 #[macro_export]
 macro_rules! csrw {
-    ($csr_number:literal, $read_register: ident) => {
+    ($csr_number:expr, $write_register: ident) => {
         /// Writes the CSR with register
         {
-            core::arch::asm!(concat!("csrw ", stringify!($csr_number), ", {}"), in(reg) $read_register);
+            core::arch::asm!(
+                "csrw {csr}, {0}", in(reg) $write_register, csr = const $csr_number
+            );
         }
     };
 }
 
 #[macro_export]
 macro_rules! csrrw {
-    ($csr_number:literal, $read_register: ident) => {
-        /// Reads and writes the CSR
+    ($csr_number:expr, $read_register: ident) => {
+        /// Reads and clears the CSR
         {
             let r: usize;
             core::arch::asm!(
-                concat!("csrrw {0}, ", stringify!($csr_number), ", {1}"),
+                concat!("csrrw {0}, {csr}, {1}"),
                 out(reg) r,
-                in(reg) $read_register
+                in(reg) $read_register,
+                csr = const $csr_number
             );
             r
         }
@@ -48,12 +52,14 @@ macro_rules! csrrw {
 
 #[macro_export]
 macro_rules! csrs {
-    ($csr_number:literal, $read_register: ident) => {
+    ($csr_number:expr, $read_register: ident) => {
         /// Sets the CSR
         {
             core::arch::asm!(
-                concat!("csrs ", stringify!($csr_number), ", {0}"),
-                in(reg) $read_register
+                "csrs {csr}, {0}",
+                in(reg) $read_register,
+                csr = const $csr_number
+
             );
         }
     };
@@ -61,12 +67,14 @@ macro_rules! csrs {
 
 #[macro_export]
 macro_rules! csrc {
-    ($csr_number:literal, $read_register: ident) => {
-        /// Clears the CSR
+    ($csr_number:expr, $read_register: ident) => {
+        /// Sets the CSR
         {
             core::arch::asm!(
-                concat!("csrc ", stringify!($csr_number), ", {0}"),
-                in(reg) $read_register
+                "csrc {csr}, {0}",
+                in(reg) $read_register,
+                csr = const $csr_number
+
             );
         }
     };
@@ -74,14 +82,15 @@ macro_rules! csrc {
 
 #[macro_export]
 macro_rules! csrrs {
-    ($csr_number:literal, $read_register: ident) => {
-        /// Reads and sets the CSR
+    ($csr_number:expr, $read_register: ident) => {
+        /// Reads and clears the CSR
         {
             let r: usize;
             core::arch::asm!(
-                concat!("csrrs {0}, ", stringify!($csr_number), ", {1}"),
+                concat!("csrrs {0}, {csr}, {1}"),
                 out(reg) r,
-                in(reg) $read_register
+                in(reg) $read_register,
+                csr = const $csr_number
             );
             r
         }
@@ -90,21 +99,20 @@ macro_rules! csrrs {
 
 #[macro_export]
 macro_rules! csrrc {
-    ($csr_number:literal, $read_register: ident) => {
+    ($csr_number:expr, $read_register: ident) => {
         /// Reads and clears the CSR
         {
             let r: usize;
             core::arch::asm!(
-                concat!("csrrc {0}, ", stringify!($csr_number), ", {1}"),
+                concat!("csrrc {0}, {csr}, {1}"),
                 out(reg) r,
-                in(reg) $read_register
+                in(reg) $read_register,
+                csr = const $csr_number
             );
             r
         }
     };
 }
-
-pub use const_format::concatcp;
 
 #[macro_export]
 macro_rules! csrwi {
@@ -124,13 +132,15 @@ macro_rules! csrwi {
 
 #[macro_export]
 macro_rules! csrrwi {
-    ($csr_number:literal, $immediate: expr) => {
+    ($csr_number:expr, $immediate: expr) => {
         /// Reads and writes the CSR with immediate
         {
             let r: usize;
             core::arch::asm!(
-                concat!("csrrwi {0}, ", stringify!($csr_number), ", ", stringify!($immediate)),
-                out(reg) r
+                concat!("csrrwi {0}, {csr}, {imm}"),
+                out(reg) r,
+                imm = const $immediate,
+                csr = const $csr_number
             );
             r
         }
@@ -139,28 +149,31 @@ macro_rules! csrrwi {
 
 #[macro_export]
 macro_rules! csrci {
-    ($csr_number:literal, $immediate: expr) => {
-        /// Clears the CSR with immediate
+    ($csr_number:expr, $immediate: expr) => {
+        /// Writes the CSR with immediate
         {
-            core::arch::asm!(concat!(
-                "csrci ",
-                stringify!($csr_number),
-                ", ",
-                stringify!($immediate)
-            ));
+            core::arch::asm!(
+                concat!(
+                    "csrci {csr}, {imm}"
+                ),
+                imm = const $immediate,
+                csr = const $csr_number
+            );
         }
     };
 }
 
 #[macro_export]
 macro_rules! csrrci {
-    ($csr_number:literal, $immediate: expr) => {
-        /// Reads and clears the CSR with immediate
+    ($csr_number:expr, $immediate: expr) => {
+        /// Reads and writes the CSR with immediate
         {
             let r: usize;
             core::arch::asm!(
-                concat!("csrrci {0}, ", stringify!($csr_number), ", ", stringify!($immediate)),
-                out(reg) r
+                concat!("csrrci {0}, {csr}, {imm}"),
+                out(reg) r,
+                imm = const $immediate,
+                csr = const $csr_number
             );
             r
         }
@@ -169,28 +182,31 @@ macro_rules! csrrci {
 
 #[macro_export]
 macro_rules! csrsi {
-    ($csr_number:literal, $immediate: expr) => {
-        /// Sets the CSR with immediate
+    ($csr_number:expr, $immediate: expr) => {
+        /// Writes the CSR with immediate
         {
-            core::arch::asm!(concat!(
-                "csrsi ",
-                stringify!($csr_number),
-                ", ",
-                stringify!($immediate)
-            ));
+            core::arch::asm!(
+                concat!(
+                    "csrwi {csr}, {imm}"
+                ),
+                imm = const $immediate,
+                csr = const $csr_number
+            );
         }
     };
 }
 
 #[macro_export]
 macro_rules! csrrsi {
-    ($csr_number:literal, $immediate: expr) => {
-        /// Reads and sets the CSR with immediate
+    ($csr_number:expr, $immediate: expr) => {
+        /// Reads and writes the CSR with immediate
         {
             let r: usize;
             core::arch::asm!(
-                concat!("csrrsi {0}, ", stringify!($csr_number), ", ", stringify!($immediate)),
-                out(reg) r
+                concat!("csrrwi {0}, {csr}, {imm}"),
+                out(reg) r,
+                imm = const $immediate,
+                csr = const $csr_number
             );
             r
         }
